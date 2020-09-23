15c4f22b600cfe06e4923ea9e31b2e8650ee66a9
matias.luna <matias.luna@codetria.com>
2020-09-23 20:34:13 -0300

    Se arranca con otro metodo pero no termina de funcionar correctamente. Se mejora el anterior.

--- src/codegolfej/Main.java
+++ src/codegolfej/Main.java
@@ -9,7 +9,7 @@ public class Main {
 		int[][] m_false = { { 1, 8, 2 }, { 0, 3, 4 }, { 7, 6, 5 } };
 		int[][] m2_false = { { 0, 1, 2, 3 }, { 4, 5, 6, 7 }, { 8, 9, 10, 11 }, { 12, 13, 14, 15 } };
 		
-		System.out.println(MatyPuzzle.validPuzzle_2Maty(m));
+		System.out.println(MatyPuzzle.validPuzzle(m_false));
 	}
 
 }
