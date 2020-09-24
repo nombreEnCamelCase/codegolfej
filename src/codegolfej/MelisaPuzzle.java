db73fa5f457c2552d515a34a9ba8358362224e6b
Mel-A <meabuin@alumno.unlam.edu.ar>
2020-09-24 00:55:18 -0300

    fix del fix del fix

--- src/codegolfej/MelisaPuzzle.java
+++ src/codegolfej/MelisaPuzzle.java
@@ -8,13 +8,13 @@ public class CodegolfMelisa {
     public static boolean isValidPuzzle(int m[][]){
         int a = 0, d, j, i =0, x, z = 0, l = m.length, t = l * l;
         while(a <t){
-            x= m[a % l][a / l];
+            x= m[a / l][a % l];
             if (x == 0)
                 z = (a % l) % 2;
             d =a+1;
             while(d < t){
-                j = m[d % l][d / l];
-                if (j != 0 && j > x)
+                j = m[d / l][d % l];
+                if (j != 0 && j < x)
                     i++;
                 d++;
             }
