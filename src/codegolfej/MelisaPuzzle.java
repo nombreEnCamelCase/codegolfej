07d524f6c5d06b7cc05ad45abc861517d1e58dde
Mel-A <meabuin@alumno.unlam.edu.ar>
2020-09-24 00:22:03 -0300

    fix

--- src/codegolfej/MelisaPuzzle.java
+++ src/codegolfej/MelisaPuzzle.java
@@ -6,25 +6,22 @@ package codegolfej;
 public class CodegolfMelisa {
 
     public static boolean isValidPuzzle(int m[][]){
-        int a = 0, d, j, i =0, x,z = 0, l = m.length, t = l * l;
-
+        int a = 0, d, j, i =0, x, l = m.length, t = l * l;
+        boolean z = false;
         while(a <t){
             x= m[a % l][a / l];
             if (x == 0)
-                z = a;
+                z = (a % l) % 2 != 0;
             d =a+1;
             while(d < t){
                 j = m[d % l][d / l];
                 if (j != 0 && j > x)
                     i++;
-                else
-                    z = d;
                 d++;
             }
             a++;
         }
         
-        return l % 2 != 0 ? i % 2 == 0 : i % 2 == z % 2;
-    }
+        return l % 2 != 0 ? i % 2 == 0 : z ? i % 2 == 0 : i % 2 != 0;
     
 }
